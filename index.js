c715009f0ef8b1d71994bb4e506dd31843f7f3b6
Rishabh Singh <124164455+DMGXI@users.noreply.github.com>
2025-02-27 18:47:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,26 @@
-let input= document.getElementById('inputbox');
-let button= document.querySelectorAll('button')
+let input = document.getElementById('inputbox');
+let buttons = document.querySelectorAll('button');
 
-let string= "";
+let string = "";
+
+buttons.forEach(button => {
+    button.addEventListener('click', (e) => {
+        if (e.target.innerText === '=') {
+            try {
+                string = eval(string);
+                input.value = string;
+            } catch {
+                input.value = "Error";
+            }
+        } else if (e.target.innerText === 'AC') {
+            string = "";
+            input.value = string;
+        } else if (e.target.innerText === 'DE') {
+            string = string.substring(0, string.length - 1);
+            input.value = string;
+        } else {
+            string += e.target.innerText;
+            input.value = string;
+        }
+    });
+});
